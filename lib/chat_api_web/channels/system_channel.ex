e54f99f79032215ae537a30305880c1535de40a8
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-10-01 15:08:19 -0600

    finish system channel functionality

--- lib/chat_api_web/channels/system_channel.ex
+++ lib/chat_api_web/channels/system_channel.ex
@@ -1,7 +1,6 @@
 defmodule ChatApiWeb.SystemChannel do
-  alias ChatApi.Token
   alias ChatApiWeb.Presence
-  alias ChatApi.Account
+  alias ChatApi.{Account, Token}
   use ChatApiWeb, :channel
 
   defp add_user_id_to_presence(socket), do: Presence.track(socket, socket.assigns.user_id, %{
@@ -14,7 +13,7 @@ defmodule ChatApiWeb.SystemChannel do
       :ok
      else
       _ ->
-        ChatApiWeb.Endpoint.broadcast!("users_socket:" <> user_id, "disconnect", %{})
+        :ok = ChatApiWeb.Endpoint.broadcast!("user_socket:" <> user_id, "disconnect", %{})
         :error
      end
   end
@@ -28,38 +27,38 @@ defmodule ChatApiWeb.SystemChannel do
   @impl true
   def handle_info(:track_user, socket) do
     if socket.assigns.hidden do
-      {:ok, _} = add_user_id_to_presence(socket)
-    else
       :ok = remove_user_id_from_presence(socket)
+    else
+      {:ok, _} = add_user_id_to_presence(socket)
     end
 
     push(socket, "presence_state", Presence.list(socket))
     {:noreply, socket}
   end
 
-  def handle_info("update_hidden_state", socket) do
+  @impl true
+  def handle_info(:update_hidden_state, socket) do
     Account.update_profile_by_user_id(socket.assigns.user_id, %{hidden: socket.assigns.hidden})
+    send(self(), :track_user)
     {:noreply, socket}
   end
 
+  @impl true
   def handle_in("set_hidden", payload, socket) do
-    authenticate(socket, socket.params["token"])
-
+    authenticate(socket, payload["token"])
     send(self(), :update_hidden_state)
-    send(self(), :track_user)
 
     {:noreply, assign(socket, :hidden, payload["hidden"])}
   end
 
+  @impl true
   def handle_in("set_display_name", payload, socket) do
-    authenticate(socket, socket.params["token"])
-
-    send(self(), :update_display_name)
+    authenticate(socket, payload["token"])
     display_name = payload["display_name"]
 
     with user when not is_nil(user) <- Account.get_user(socket.assigns.user_id) do
       if user.display_name == display_name do
-        {:reply, :display_name_unchanged, socket}
+        {:reply, {:error, :display_name_unchanged}, socket}
       else
         case Account.update_display_name(user, display_name) do
           {:ok, updated_user} ->
@@ -68,17 +67,9 @@ defmodule ChatApiWeb.SystemChannel do
               display_name: updated_user.display_name
             })
             {:noreply, socket}
-          error -> {:reply, error, socket}
+          error -> {:reply, {:error, error}, socket}
         end
       end
     end
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (system:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
   end
 end
