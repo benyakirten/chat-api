19af6cf321ba35ba847ed7c41c1999aec6b03760
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-11-23 20:29:55 -0700

    fix call of MEssage.paginate_messages_query in Conversation.user_conversation_with_details_query

--- lib/chat_api/chat/conversation.ex
+++ lib/chat_api/chat/conversation.ex
@@ -131,7 +131,7 @@ defmodule ChatApi.Chat.Conversation do
 
   @spec user_conversation_with_details_query(binary(), binary()) :: Ecto.Query.t()
   def user_conversation_with_details_query(conversation_id, user_id) do
-    {messages_query, _page_size} = Message.paginate_messages_query()
+    {messages_query, _page_size} = Message.paginate_messages_query(conversation_id)
 
     from(
       c in Conversation,
