240e13325e949eaf6a247c839e42b3bd419c2bcc
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2023-11-26 04:16:11 -0700

    fix indexes ordering

--- priv/repo/migrations/20231113012406_add_pagination.exs
+++ priv/repo/migrations/20231113012406_add_pagination.exs
@@ -2,8 +2,8 @@ defmodule ChatApi.Repo.Migrations.AddPagination do
   use Ecto.Migration
 
   def change do
-    create(index(:users, [:inserted_at, :id]))
-    create(index(:conversations, [:inserted_at, :id]))
-    create(index(:messages, [:inserted_at, :id]))
+    create(index(:users, ["inserted_at DESC", "id DESC"]))
+    create(index(:conversations, ["inserted_at DESC", "id DESC"]))
+    create(index(:messages, ["inserted_at DESC", "id DESC"]))
   end
 end
